189bb2e0b912fd685405f1cfb607c85fc5818fbe
Ryan O'Connor <rocsteady888@gmail.com>
2019-06-06 22:32:57 -0400

    added component layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Dashboard from './components/content/Dashboard'
+import Landing from './components/content/Landing'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+
 
 function App() {
   return (
-    <div className="App">
-     <h1>Cellfie</h1>
-    </div>
+    <Router>
+      <React.Fragment>
+        <Route exact path='/dashboard' component={Dashboard} />
+        <Route exact path='/' component={Landing} />
+        <section>
+          <Switch>
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/register' component={Register} />
+          </Switch>
+        </section>
+      </React.Fragment>
+    </Router>
   );
 }
 
