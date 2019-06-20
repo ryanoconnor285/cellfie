1b47e4ef450fd67e760bcd5db6635cf3a400e0f4
Ryan O'Connor <rocsteady888@gmail.com>
2019-06-20 16:27:00 -0400

    initial state for user should be an empty object

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -3,7 +3,7 @@ import React, { useState, createContext } from 'react'
 export const UserContext = createContext({})
 
 export const UserProvider = ( {children} ) => {
-    const [user, setUser] = useState(null)
+    const [user, setUser] = useState({})
     return <UserContext.Provider value={ [user, setUser] }>
         {children}
     </UserContext.Provider>
