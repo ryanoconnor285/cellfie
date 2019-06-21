f2a44aeccc1938bf46c0bf0c1d3580b4134a9260
Ryan O'Connor <rocsteady888@gmail.com>
2019-06-21 12:57:37 -0400

    added run provider, removed dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
-import React from 'react'
-import { BrowserRouter as Router, Route} from 'react-router-dom'
-import { UserProvider } from './context/UserContext'
-import Landing from './components/content/Landing'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { UserProvider } from "./context/UserContext";
+import { RunProvider } from "./context/RunContext";
+import Landing from "./components/content/Landing";
 import Navbar from "./components/navigation/Navbar";
-
 
 function App() {
   return (
     <UserProvider>
-      <Router>
-        <React.Fragment>
-          <Navbar />
-          <Route exact path='/' component={Landing} />
-        </React.Fragment>
-      </Router>
+      <RunProvider>
+        <Router>
+          <React.Fragment>
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+          </React.Fragment>
+        </Router>
+      </RunProvider>
     </UserProvider>
   );
 }
