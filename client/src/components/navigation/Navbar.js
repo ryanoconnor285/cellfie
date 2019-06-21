35ae94badabb206871b46bc445112f08ed70df43
Ryan O'Connor <rocsteady888@gmail.com>
2019-06-21 12:58:43 -0400

    moved navigation to the right

--- client/src/components/navigation/Navbar.js
+++ client/src/components/navigation/Navbar.js
@@ -1,61 +1,66 @@
-import React, { useState } from 'react'
-import { Menu, Segment } from 'semantic-ui-react'
+import React, { useState } from "react";
+import { Menu, Segment, Header } from "semantic-ui-react";
 
 const Navbar = () => {
-    const [activeItem, setActiveItem] = useState('landing')
-    
-    return (
-        <Segment inverted>
-          <Menu inverted pointing secondary>
-            <Menu.Item
-            name='landing'
-            active={activeItem === 'landing'}
-            onClick={(e) => setActiveItem('landing')}
-            >
-            Cellfie
-            </Menu.Item>
-            <Menu.Item
-            name='downloadTestDataset'
-            active={activeItem === 'downloadTestDataset'}
-            onClick={(e) => setActiveItem('downloadTestDataset')}
-            >
+  const [activeItem, setActiveItem] = useState("landing");
+
+  return (
+    <Segment inverted>
+      <Menu inverted pointing secondary>
+        <Menu.Menu size="massive">
+          <Menu.Item
+            name="landing"
+            active={activeItem === "landing"}
+            onClick={e => setActiveItem("landing")}
+          >
+            <Header inverted as='h1'>Cellfie</Header>
+          </Menu.Item>
+        </Menu.Menu>
+
+        <Menu.Menu position="right">
+          <Menu.Item
+            name="downloadTestDataset"
+            active={activeItem === "downloadTestDataset"}
+            onClick={e => setActiveItem("downloadTestDataset")}
+          >
             Download Test Dataset
-            </Menu.Item>
+          </Menu.Item>
 
-            <Menu.Item 
-            name='exampleResults' 
-            active={activeItem === 'exampleResults'} 
-            onClick={e => setActiveItem('exampleResults')}
-            >
+          <Menu.Item
+            name="exampleResults"
+            active={activeItem === "exampleResults"}
+            onClick={e => setActiveItem("exampleResults")}
+          >
             Example Results
-            </Menu.Item>
+          </Menu.Item>
 
-            <Menu.Item
-            name='documentation'
-            active={activeItem === 'documentation'}
-            onClick={e => setActiveItem('documentation')}
-            >
+          <Menu.Item
+            name="documentation"
+            active={activeItem === "documentation"}
+            onClick={e => setActiveItem("documentation")}
+          >
             Documentation
-            </Menu.Item>
+          </Menu.Item>
 
-            <Menu.Item
-            name='contact'
-            active={activeItem === 'contact'}
-            onClick={e => setActiveItem('contact')}
-            >
+          <Menu.Item
+            name="contact"
+            active={activeItem === "contact"}
+            onClick={e => setActiveItem("contact")}
+          >
             Contact Us
-            </Menu.Item>
+          </Menu.Item>
 
-            <Menu.Item
-            name='reportBug'
-            active={activeItem === 'reportBug'}
-            onClick={e => setActiveItem('reportBug')}
-            >
+          <Menu.Item
+            name="reportBug"
+            active={activeItem === "reportBug"}
+            onClick={e => setActiveItem("reportBug")}
+          >
             Report a Bug
-            </Menu.Item>
-          </Menu>
-        </Segment>
-    )
-}
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+    </Segment>
+  );
+};
 
-export default Navbar
+export default Navbar;
