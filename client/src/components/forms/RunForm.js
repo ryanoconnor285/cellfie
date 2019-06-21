0d05b16dae4b6350dafc531fe0ba4ccb836b5bc8
Ryan O'Connor <rocsteady888@gmail.com>
2019-06-21 12:58:19 -0400

    now renders a tabbed multi step form

--- client/src/components/forms/RunForm.js
+++ client/src/components/forms/RunForm.js
@@ -1,63 +1,49 @@
-import React, { useState, useContext } from "react";
+import React, { Fragment, useState } from "react";
+import EmailInputForm from "./EmailInputForm";
+import FileUploadForm from "./FileUploadForm";
+import SampleInformation from "./SampleInformation";
+import { Menu, Segment } from "semantic-ui-react";
 
-import { UserContext } from '../../context/UserContext';
-import axios from 'axios';
-import { Button, Form, Message } from "semantic-ui-react";
+function RunForm() {
+  const [activeItem, setActiveItem] = useState("0");
 
-function RunForm(){
-  const [, setUser] = useContext(UserContext);
-  const [formData, setFormData] = useState({
-    email: "",
-    runName: ""
-  });
-  const [errors, setErrors] = useState({});
-
-  const handleChange = e => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async e => {
-    e.preventDefault();
-    const 
-    const res = await axios
-      .post("http://cellfie2.renci.org:8080/cellfie/0.0.3/run", )
-  
-      if (res) {
-        return console.log(res)
-      }
-      console.log(formData)
-  };
+  const handleItemClick = (e, { value }) => setActiveItem(value);
 
   return (
-    <Form error>
-
-      <Form.Input name="email" label="Email" placeholder="joe@email.com" onChange={handleChange}/>
-      {
-        errors.email
-        ?
-        <Message
-          error
-          header="Email Required"
-          content="Your email will be used to find your data later."
+    <Fragment>
+      <Menu attached="top" tabular>
+        <Menu.Item
+          name="Basic Info"
+          value="0"
+          active={activeItem === "0"}
+          onClick={handleItemClick}
         />
-        :
-        null
-      }
-
-      <Form.Input name="runName" label="Run Name" placeholder="rat samples" onChange={handleChange}/>
-      {
-        errors.runName
-        ?
-        <Message
-          error
-          header="Run Name"
-          content="You will need this to keep runs organized."
+        <Menu.Item
+          name="File Uploader"
+          value="1"
+          active={activeItem === "1"}
+          onClick={handleItemClick}
         />
-        :
-        null
-      }
-      <Button onClick={handleSubmit}>Submit</Button>
-    </Form>
+        <Menu.Item
+          name="Sample Information"
+          value="2"
+          active={activeItem === "2"}
+          onClick={handleItemClick}
+        />
+        <Menu.Item
+          name="Library and Parameters"
+          value="3"
+          active={activeItem === "3"}
+          onClick={handleItemClick}
+        />
+      </Menu>
+
+      <Segment attached="bottom">
+        {activeItem === '0' && <EmailInputForm />}
+        {activeItem === '1' && <FileUploadForm />}
+        {activeItem === '2' && <SampleInformation />}
+      </Segment>
+    </Fragment>
   );
 }
 
