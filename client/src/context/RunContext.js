16444dc4b4458a56f3b070fcf88c1d0b214af8ce
Ryan O'Connor <rocsteady888@gmail.com>
2019-06-23 16:08:01 -0400

    initial commit

--- client/src/context/RunContext.js
+++ client/src/context/RunContext.js
@@ -3,7 +3,7 @@ import React, { useState, createContext } from "react";
 export const RunContext = createContext({});
 
 export const RunProvider = ({ children }) => {
-  const [run, setRun] = useState(null);
+  const [run, setRun] = useState({});
   return (
     <RunContext.Provider value={[run, setRun]}>{children}</RunContext.Provider>
   );
