51ed51785909cbdbd7ffd38215c3a8cb0b012fbd
Ryan O'Connor <rocsteady888@gmail.com>
2019-07-15 14:08:05 -0400

    corrected the input values from pinaple py

--- client/src/components/forms/ParametersForm.js
+++ client/src/components/forms/ParametersForm.js
@@ -1,96 +1,63 @@
-import React, { Fragment, useState, useContext } from "react";
-import LibraryOptions from "./dropdowns/LibraryOptions";
+import React, { Fragment, useContext } from "react";
 import { RunContext } from "../../context/RunContext";
-import { Form, Button } from "semantic-ui-react";
+import { Form } from "semantic-ui-react";
 
 function FormExampleWidthField() {
   const [run, setRun] = useContext(RunContext);
-  const [toggleAdvanced, setToggleAdvanced] = useState(false);
 
-  const screenTypeOptions = [
-    { key: "enrichment", text: "Enrichment", value: "enrichment" },
-    { key: "depletion", text: "Depletion", value: "depletion" }
+  const geneThresholdOptions = [
+    { key: "global", text: "Global", value: "global" },
+    { key: "local", text: "Local", value: "local" }
   ];
 
-  const handleChange = (e, { value }) => {
-    setRun({ ...run, [e.target.name]: value });
+  const handleChange = e => {
+    setRun({ ...run, [e.target.name]: e.target.value });
     console.log([e.target.name]);
+  };
+
+  const handleDropdownChange = (e, { name, value}) => {
+    setRun({ ...run, [name]: value });
+    console.log(run)
   };
 
   return (
     <Form>
       <h1>Configure your analysis</h1>
       <h5>
-        Please choose your screen type and the library used in your screen
-      </h5>
-      <h5>
-        If you wish to edit the default parameter seetings, click "Advanced
-        Options"
+        Please choose your the options for the definition of gene activity threshold:
       </h5>
       <h4>Project Parameters</h4>
       <Form.Group>
         <Form.Dropdown
-          label="Screen Type"
-          name="screenType"
+          label="Gene Threshold"
+          name="geneThreshold"
           selection
-          placeholder="Screen Type"
-          options={screenTypeOptions}
+          placeholder="Gene Threshold"
+          options={geneThresholdOptions}
           width={4}
-          value={run.screenType}
+          onChange={handleDropdownChange}
+        />
+        <Form.Input 
+          label="Total Samples"
+          name="totalSamples"
+          placeholder="Total Samples" 
+          width={2} 
           onChange={handleChange}
         />
-        <LibraryOptions setRun={setRun} run={run} />
-        {run.library === "customLibrary" ? (
-          <Form.Group>
-            <Form.Input placeholder="Library" width={2} />
-            <Form.Input placeholder="12 Wide" width={12} />
-            <Form.Input placeholder="2 Wide" width={2} />
-          </Form.Group>
-        ) : null}
       </Form.Group>
-      <Form.Group>
-        <Button onClick={() => setToggleAdvanced(!toggleAdvanced)}>
-          Advanced Options
-        </Button>
-      </Form.Group>
-      {toggleAdvanced ? (
+      {run.geneThreshold === 'global' ? (
         <Fragment>
-          <h4>Alignment</h4>
+          <h4>Global Threshold Options</h4>
           <Form.Group>
             <Form.Input label="sgRNA Length (bp)" placeholder="sgRNA Length (bp)" width={2} />
-            <Form.Input label="Adapter Error Rate" placeholder="Adapter Error Rate" width={2} />
-            <Form.Input label="Matching Threshold" placeholder="Matching Threshold" width={2} />
-            <Form.Input label="Ambiguity Threshold" placeholder="Ambiguity Threshold" width={2} />
-            <Form.Input label="Seed Length" placeholder="Seed Length" width={2} />
-            <Form.Dropdown label="Seed Number" placeholder="Seed Number" width={2} />
-            <Form.Input label="Interval Function" placeholder="Interval Function" width={2} />
           </Form.Group>
-
-          <h4>Read Counting</h4>
+        </Fragment>
+      ) : null}
+      {run.geneThreshold === 'local' ? (
+        <Fragment>
+          <h4>Local Threshold Options</h4>
           <Form.Group>
-            <Form.Input placeholder="Normalization" width={8} />
-            <Form.Input placeholder="Cutoff" width={6} />
-            <Form.Input placeholder="Round Counts" width={2} />
-            <Form.Input placeholder="Averaging" width={6} />
-          </Form.Group>
-
-          <h4>Gene Ranking</h4>
-          <Form.Group>
-            <Form.Input placeholder="Gene Ranking Metric" width={8} />
-            <Form.Input placeholder="Number of permutations" width={6} />
-            <Form.Input placeholder="sgRNA p-value Threshold (aRRA only)" width={2} />
-            <Form.Input placeholder="% sgRNAs Included (STARS only)" width={6} />
-          </Form.Group>
-
-          <h4>Statistical Significance</h4>
-          <Form.Group>
-            <Form.Input placeholder="Signif. level (sgRNA Ranking)" width={8} />
-            <Form.Input placeholder="Signif. Level (Gene Ranking)" width={6} />
-            <Form.Input placeholder="p-value Adjustment" width={2} />
-            <Form.Input
-              placeholder="Signif. Level (Model Selection)"
-              width={6}
-            />
+            <Form.Input label="sgRNA Length (bp)" placeholder="sgRNA Length (bp)" width={2} />
           </Form.Group>
         </Fragment>
       ) : null}
