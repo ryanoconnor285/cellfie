1f19aedf1782726487c81ac0ba4a38a7d290e46a
Ryan O'Connor <rocsteady888@gmail.com>
2019-07-18 17:48:28 -0400

    added next and back buttons

--- client/src/components/forms/RunForm.js
+++ client/src/components/forms/RunForm.js
@@ -2,10 +2,10 @@ import React, { Fragment, useState } from "react";
 import EmailInputForm from "./EmailInputForm";
 import FileUploadForm from "./FileUploadForm";
 import ParametersForm from "./ParametersForm";
-import { Menu, Segment } from "semantic-ui-react";
+import { Menu, Button, Segment } from "semantic-ui-react";
 
 function RunForm() {
-  const [activeItem, setActiveItem] = useState("0");
+  const [activeItem, setActiveItem] = useState(0);
 
   const handleItemClick = (e, { value }) => setActiveItem(value);
 
@@ -14,29 +14,33 @@ function RunForm() {
       <Menu attached="top" tabular>
         <Menu.Item
           name="Basic Info"
-          value="0"
-          active={activeItem === "0"}
+          value={0}
+          active={activeItem === 0}
           onClick={handleItemClick}
         />
         <Menu.Item
           name="File Uploader"
-          value="1"
-          active={activeItem === "1"}
+          value={1}
+          active={activeItem === 1}
           onClick={handleItemClick}
         />
         <Menu.Item
           name="Library and Parameters"
-          value="2"
-          active={activeItem === "2"}
+          value={2}
+          active={activeItem === 2}
           onClick={handleItemClick}
         />
       </Menu>
 
       <Segment attached="bottom">
-        {activeItem === '0' && <EmailInputForm />}
-        {activeItem === '1' && <FileUploadForm />}
-        {activeItem === '2' && <ParametersForm />}
+        {activeItem === 0 && <EmailInputForm />}
+        {activeItem === 1 && <FileUploadForm />}
+        {activeItem === 2 && <ParametersForm />}
       </Segment>
+      <Menu>
+        <Button onClick={() => setActiveItem(activeItem - 1)} disabled={activeItem <= 0}>Back</Button>
+        <Button onClick={() => setActiveItem(activeItem + 1)} disabled={activeItem >= 2}>Next</Button>
+      </Menu>
     </Fragment>
   );
 }
