a84a5de76bff87b1f1f71a1fe946819a9b3fd6bb
Ryan O'Connor <rocsteady888@gmail.com>
2019-07-19 13:29:42 -0400

    added logic for updated global and local options

--- client/src/components/forms/ParametersForm.js
+++ client/src/components/forms/ParametersForm.js
@@ -1,23 +1,44 @@
-import React, { Fragment, useContext } from "react";
+import React, { Fragment, useState, useContext } from "react";
 import { RunContext } from "../../context/RunContext";
-import { Form } from "semantic-ui-react";
+import { Form, Button } from "semantic-ui-react";
 
 function FormExampleWidthField() {
   const [run, setRun] = useContext(RunContext);
+  const [gloOptions, setGloOptions] = useState({
+    id: 0,
+    thresholdType: "percentile",
+    thresholdValue: 75
+  });
+  const [locOptions, setLocOptions] = useState({
+    id: 0,
+    moreThanTwoSamples: false,
+    thresholdMethod: "mean",
+    minimum: 25,
+    maximum: 75,
+    customLibraryUrl: "string"
+  });
 
-  const geneThresholdOptions = [
-    { key: "global", text: "Global", value: "global" },
-    { key: "local", text: "Local", value: "local" }
-  ];
-
-  const handleChange = e => {
-    setRun({ ...run, [e.target.name]: e.target.value });
-    console.log([e.target.name]);
+  const handleChange = (e, { name, value }) => {
+    setRun({ ...run, [name]: value });
+    console.log(run);
   };
 
-  const handleDropdownChange = (e, { name, value}) => {
-    setRun({ ...run, [name]: value });
-    console.log(run)
+  const handleSetGlobalOptions = (e, { name, value }) => {
+    setGloOptions({ ...gloOptions, [name]: value });
+  };
+
+  const handleSetLocalOptions = (e, { name, value }) => {
+    setLocOptions({ ...locOptions, [name]: value });
+  };
+
+  const handleGlobalOptions = () => {
+    setRun({ ...run, globalOptions: gloOptions });
+    console.log(run);
+  };
+
+  const handleLocalOptions = () => {
+    setRun({ ...run, localOptions: locOptions });
+    console.log(run);
   };
 
   return (
@@ -26,39 +47,97 @@ function FormExampleWidthField() {
       <h5>
         Please choose your the options for the definition of gene activity threshold:
       </h5>
-      <h4>Project Parameters</h4>
+      <h3>Project Parameters</h3>
       <Form.Group>
         <Form.Dropdown
           label="Gene Threshold"
           name="geneThreshold"
+          value={run.geneThreshold ? run.geneThreshold : ""}
           selection
           placeholder="Gene Threshold"
-          options={geneThresholdOptions}
-          width={4}
-          onChange={handleDropdownChange}
+          options={[
+            { key: "global", text: "Global", value: "global" },
+            { key: "local", text: "Local", value: "local" }
+          ]}
+          onChange={handleChange}
         />
-        <Form.Input 
+        <Form.Input
           label="Total Samples"
           name="totalSamples"
-          placeholder="Total Samples" 
-          width={2} 
+          value={run.totalSamples ? run.totalSamples : ""}
+          placeholder="Total Samples"
           onChange={handleChange}
         />
       </Form.Group>
-      {run.geneThreshold === 'global' ? (
+      {run.geneThreshold === "global" ? (
         <Fragment>
           <h4>Global Threshold Options</h4>
           <Form.Group>
-            <Form.Input label="sgRNA Length (bp)" placeholder="sgRNA Length (bp)" width={2} />
+            <Form.Dropdown
+              label="Threshold Type"
+              name="thresholdType"
+              selection
+              placeholder="Threshold Type"
+              options={[
+                { key: "percentile", text: "Percentile", value: "percentile" },
+                { key: "value", text: "Value", value: "value" }
+              ]}
+              onChange={handleSetGlobalOptions}
+            />
+            <Form.Input
+              label="Threshold Value"
+              name="thresholdValue"
+              placeholder="Threshold Value"
+              icon={run.globalOptions.thresholdType === "percentile" ? 'percent': ''}
+              onChange={handleSetGlobalOptions}
+            />
           </Form.Group>
+          <Button onClick={handleGlobalOptions}>Set Options</Button>
         </Fragment>
       ) : null}
-      {run.geneThreshold === 'local' ? (
+      {run.geneThreshold === "local" ? (
         <Fragment>
           <h4>Local Threshold Options</h4>
           <Form.Group>
-            <Form.Input label="sgRNA Length (bp)" placeholder="sgRNA Length (bp)" width={2} />
+            <Form.Dropdown
+              label="Threshold Method"
+              name="thresholdMethod"
+              selection
+              placeholder="Threshold Method"
+              options={[
+                { key: "minMaxMean", text: "MinMaxMean", value: "minMaxMean" },
+                { key: "mean", text: "Mean", value: "mean" },
+                { key: "custom", text: "Custom", value: "custom" }
+              ]}
+              onChange={handleSetLocalOptions}
+            />
           </Form.Group>
+          <Form.Group>
+            <Form.Dropdown
+              label="Threshold Type"
+              name="thresholdType"
+              selection
+              placeholder="Threshold Type"
+              options={[
+                { key: "percentile", text: "Percentile", value: "percentile" },
+                { key: "value", text: "Value", value: "value" }
+              ]}
+              onChange={handleSetLocalOptions}
+            />
+            <Form.Input
+              label="Minimum"
+              name="minimum"
+              placeholder="Percent (0 - 95%)"
+              onChange={handleSetLocalOptions}
+            />
+            <Form.Input
+              label="Maximum"
+              name="maximum"
+              placeholder="Percent (5 - 100%)"
+              onChange={handleSetLocalOptions}
+            />
+          </Form.Group>
+          <Button onClick={handleLocalOptions}>Set Options</Button>
         </Fragment>
       ) : null}
     </Form>
