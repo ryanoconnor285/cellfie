860f40ab5763841688ebb2194619612d63b5cd85
Ryan O'Connor <rocsteady888@gmail.com>
2019-07-19 13:28:34 -0400

    replaced grid with conatiner

--- client/src/components/content/Landing.js
+++ client/src/components/content/Landing.js
@@ -1,17 +1,13 @@
 import React from "react";
 
-import { Grid } from 'semantic-ui-react'
+import { Container } from "semantic-ui-react";
 import RunForm from "../forms/RunForm";
 
 const Landing = () => {
   return (
-    <Grid centered columns={1} divided>
-      <Grid.Row>
-        <Grid.Column width={10}>
-          <RunForm />
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
+    <Container>
+      <RunForm />
+    </Container>
   );
 };
 
