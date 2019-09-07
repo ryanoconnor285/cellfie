5303d0ba81e42f8351ec0d02a92b74172b1bc87e
Ryan O'Connor <rocsteady888@gmail.com>
2019-09-06 21:26:14 -0400

    removed table

--- client/src/components/forms/FileUploadForm.js
+++ client/src/components/forms/FileUploadForm.js
@@ -1,8 +1,7 @@
 import React, { useState, useContext } from "react";
-
 import Papa from 'papaparse';
 import { RunContext } from "../../context/RunContext";
-import { Form, Table, Radio, Input } from "semantic-ui-react";
+import { Form } from "semantic-ui-react";
 
 function FileUploadForm() {
   const [run, setRun] = useContext(RunContext);
@@ -11,21 +10,18 @@ function FileUploadForm() {
   //TODO handle setErrors if user enters an invalid email
   // const [errors, setErrors] = useState({});
 
-  const handleChange = e => {
-    setRun({ ...run, [e.target.name]: e.target.value });
-  };
 
   const handleFileSelect = e => {
     Papa.parse(e.target.files[0], {
       complete: function(results) {
         setFile(results);
         console.log(results);
+        setRun({ ...run, totalSamples: results.data.length-1 });
       }
     });
   }
 
   return (
-    <Form error>
       <Form.Field>
         <Form.Input
           type="file"
@@ -33,27 +29,6 @@ function FileUploadForm() {
           onChange={handleFileSelect}
         />
       </Form.Field>
-      <h1>Define your samples</h1>
-      <Table celled>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell width={6}>Sample</Table.HeaderCell>
-            <Table.HeaderCell width={1}>Control</Table.HeaderCell>
-            <Table.HeaderCell width={5}>Condition Name</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {file ? file.data[0].map((sample, index) => 
-          <Table.Row key={index}>
-            <Table.Cell>{sample}</Table.Cell>
-            <Table.Cell><Radio toggle/></Table.Cell>
-            <Table.Cell><Input fluid name="conditionName" value={run.conditionName ? run.conditionName : ""} placeholder='ToxinA...' 
-        onChange={handleChange} /></Table.Cell>
-          </Table.Row>
-          ) : null}
-        </Table.Body>
-      </Table>
-    </Form>
   );
 }
 
