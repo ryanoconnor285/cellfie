69918b39e722a666f774602b7f4f1a8bd4047b78
Ryan O'Connor <rocsteady888@gmail.com>
2019-09-06 21:28:38 -0400

    now handles reference selection

--- client/src/components/forms/ParametersForm.js
+++ client/src/components/forms/ParametersForm.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import FileUploadForm from "./FileUploadForm";
 import GlobalThreshold from "./threshold/GlobalThreshold";
 import LocalThreshold from "./threshold/LocalThreshold";
 import { RunContext } from "../../context/RunContext";
@@ -12,6 +13,21 @@ function FormExampleWidthField() {
     console.log(run);
   };
 
+  const referenceModelOptions = [
+    {
+      key: "human_Recon2_2",
+      text: "Human (Recon2.2)",
+      value: "human_Recon2_2"
+    },
+    { key: "human_Recon2", text: "Human (Recon2)", value: "human_Recon2" },
+    { key: "human_Recon1", text: "Human (Recon1)", value: "human_Recon1" },
+    { key: "human_ihsa", text: "Human (iHsa)", value: "human_ihsa" },
+    { key: "human_quek14", text: "Human (quek14)", value: "human_quek14" },
+    { key: "rat_irno", text: "Rat (iRno)", value: "rat_irno" },
+    { key: "mouse_imm1415", text: "Mouse (iMM1415)", value: "mouse_imm1415" },
+    { key: "cho_icho_v1", text: "CHO (iCHOv1)", value: "cho_icho_v1" }
+  ];
+
   return (
     <Form>
       <h1>Configure your analysis</h1>
@@ -19,6 +35,18 @@ function FormExampleWidthField() {
         Please choose your the options for the definition of gene activity
         threshold:
       </h5>
+      <h3>Reference Model</h3>
+      <Form.Dropdown
+        label="Reference Model"
+        name="referenceModel"
+        selection
+        placeholder="Reference Model"
+        options={referenceModelOptions}
+        width={6}
+        onChange={handleChange}
+      />
+      <h3>File Upload</h3>
+      <FileUploadForm />
       <h3>Project Parameters</h3>
       <Form.Group>
         <Form.Dropdown
@@ -36,9 +64,8 @@ function FormExampleWidthField() {
         <Form.Input
           label="Total Samples"
           name="totalSamples"
-          value={run.totalSamples ? run.totalSamples : ""}
+          value={run.totalSamples}
           placeholder="Total Samples"
-          onChange={handleChange}
         />
       </Form.Group>
       {run.geneThreshold === "global" ? <GlobalThreshold /> : null}
