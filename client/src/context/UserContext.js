849bcb2ee65a33dd7b270ead3078562fb5228077
Ryan O'Connor <rocsteady888@gmail.com>
2019-09-09 12:30:45 -0400

    default email should be empty string

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -3,7 +3,7 @@ import React, { useState, createContext } from 'react'
 export const UserContext = createContext({})
 
 export const UserProvider = ( {children} ) => {
-    const [user, setUser] = useState({})
+    const [user, setUser] = useState({email: ""})
     return <UserContext.Provider value={ [user, setUser] }>
         {children}
     </UserContext.Provider>
