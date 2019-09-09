fbddd79c6d04f116b39b5464a5e5626c35c3d826
Ryan O'Connor <rocsteady888@gmail.com>
2019-09-09 12:29:19 -0400

    now only handles email input from user

--- client/src/components/forms/EmailInputForm.js
+++ client/src/components/forms/EmailInputForm.js
@@ -1,30 +1,23 @@
 import React, { useState, useContext } from "react";
-
 import { UserContext } from "../../context/UserContext";
-import { RunContext } from "../../context/RunContext";
 import { Form, Message } from "semantic-ui-react";
 
 function EmailInputForm() {
-  const [, setUser] = useContext(UserContext);
-  const [run, ] = useContext(RunContext);
-  const [formData] = useState({
-    contact: "",
-    name: ""
-  });
+  const [user, setUser] = useContext(UserContext);
 
   //TODO handle setErrors if user enters an invalid email
   const [errors] = useState({});
 
   const handleChange = e => {
-    setUser({ email: formData.email });
-    console.log(run)
+    setUser({ [e.target.name]: e.target.value });
+    console.log(user);
   };
 
   return (
-    <Form width={6} error>
+    <>
       <Form.Input
-        name="contact"
-        value={run.contact ? run.contact : ""}
+        name="email"
+        value={user.email}
         label="Email"
         width={6}
         placeholder="joe@email.com"
@@ -37,23 +30,7 @@ function EmailInputForm() {
           content="Your email will be used to find your data later."
         />
       ) : null}
-
-      <Form.Input
-        name="name"
-        value={run.name ? run.name : ""}
-        label="Run Name"
-        width={6}
-        placeholder="Run Name"
-        onChange={handleChange}
-      />
-      {errors.name ? (
-        <Message
-          error
-          header="Run Name"
-          content="You will need this to keep runs organized."
-        />
-      ) : null}
-    </Form>
+    </>
   );
 }
 
