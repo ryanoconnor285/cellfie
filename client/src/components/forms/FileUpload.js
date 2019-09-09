040253e33cc318ded6f785f112a3537861c0400e
Ryan O'Connor <rocsteady888@gmail.com>
2019-09-09 12:29:28 -0400

    intiial commit

---
+++ client/src/components/forms/FileUpload.js
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+import Papa from 'papaparse';
+import { RunContext } from "../../context/RunContext";
+import { FileContext } from "../../context/FileContext";
+import { Form } from "semantic-ui-react";
+
+function FileUploadForm() {
+  const [run, setRun] = useContext(RunContext);
+  const [file, setFile] = useContext(FileContext);
+
+  const handleFileSelect = e => {
+    setFile(e.target.files[0]);
+
+    Papa.parse(e.target.files[0], {
+      complete: function(results) {
+        console.log(results);
+        setRun({ ...run, totalSamples: results.data.length-1, file:file });
+      }
+    });
+  }
+
+  return (
+      <Form.Field>
+        <Form.Input
+          type="file"
+          width={6}
+          onChange={handleFileSelect}
+        />
+      </Form.Field>
+  );
+}
+
+export default FileUploadForm;
