86fbc5fbac55e454e12ba886f9440752a2fbc0be
Ryan O'Connor <rocsteady888@gmail.com>
2019-09-09 12:30:58 -0400

    added route for uploading a file

--- server/index.js
+++ server/index.js
@@ -1,34 +1,68 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-
-const upload = require('./routes/api/upload');
-
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const uniqid = require("uniqid");
+const fs = require("fs.extra");
+const request = require("request");
+const port = 5000;
 const app = express();
 
-// Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(fileUpload());
 
-// Use Routes
-app.use('/api/upload', upload);
+// Upload Endpoint
+app.post("/upload", (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ msg: "No file uploaded" });
+  }
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+  const file = req.files.file;
+
+  // File ID with User ID and Unique Global ID (uniqid)
+  const guid = uniqid();
+  // TODO implement json web tokens
+  const uid = req.body.uid;
+  console.log(uid)
+  const fid = `/var/sysbio/cellfie/local/data/${uid}/${guid}`;
+
+  fs.mkdirp(`${__dirname}/${fid}/in.csv`, err => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
+    }
+
+    const model = "MT_recon_2_2_entrez.mat";
+    const thresh = "-l";
+    const percval = "-v";
+    const type = "minmaxmean";
+    const low = 25;
+    const high = 75;
+
+    request(
+      `curl -X POST "http://cellfie2.renci.org:8080/tx-queue/2/scheduler/job" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"image\":\"krobasky/cellfiemodels:nooptV1\",\"command\":\"/bin/bash -c './execCellfie.sh -d tmp/in.csv -r\
+  ${model} ${thresh} ${percval} -t ${type} --low ${low} --high ${high} >  tmp/output.tsv 2> tmp/err.tsv'\",\"mounts\":[{\"source\":\"${fid}\",\"target\":\"/CellFie/tmp\",\"type\":\"bind\",\"read_only\":false}]}"|sed 's/\"//g'`,
+      function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+          // writing the response to a file named data.html
+          fs.writeFileSync("data.html", body);
+        }
+      }
+    );
+    console.log(fid);
+
+    res.json({ fileName: file.name, filePath: fid, guid: guid});
+  });
 });
 
-// Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+app.post("/schedule", (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ msg: "No file uploaded" });
+  }
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+  const file = req.files.file;
 
-const port = process.env.PORT || 5000;
+  // File ID with User ID and Unique Global ID (uniqid)
+  const guid = uniqid();
+  const uid = "kimberly..email.com";
+  const fid = `/var/sysbio/cellfie/local/data/${uid}/${guid}/${file}`;
+});
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`App listening ${port}`));
