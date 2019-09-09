da0896faaad775c77aedc4c9309e7440b02d6b6b
Ryan O'Connor <rocsteady888@gmail.com>
2019-09-09 12:30:20 -0400

    initial commit

---
+++ client/src/context/FileContext.js
@@ -0,0 +1,10 @@
+import React, { useState, createContext } from 'react'
+
+export const FileContext = createContext({})
+
+export const FileProvider = ( {children} ) => {
+    const [file, setFile] = useState({})
+    return <FileContext.Provider value={ [file, setFile] }>
+        {children}
+    </FileContext.Provider>
+}
