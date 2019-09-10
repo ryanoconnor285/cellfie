369dfef02e0377832ae677a02ca1ede2de836c2c
Ryan O'Connor <rocsteady888@gmail.com>
2019-09-10 14:41:34 -0400

    initial commit

---
+++ client/src/components/content/InfoPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Image } from 'semantic-ui-react'
+
+const InfoPage = () => {
+  return (
+    <div>
+      <Image src='/images/wireframe/square-image.png' size='medium' circular />
+      Set Parameters
+      <Image src='/images/wireframe/square-image.png' size='medium' circular />
+      Upload Samples
+      <Image src='/images/wireframe/square-image.png' size='medium' circular />
+      
+    </div>
+  )
+}
+
+export default InfoPage
