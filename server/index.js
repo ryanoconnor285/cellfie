6333b72e466067e6be677013038ee5901025fe79
Ryan O'Connor <rocsteady888@gmail.com>
2019-09-10 12:38:10 -0400

    check for production

--- server/index.js
+++ server/index.js
@@ -16,14 +16,9 @@ app.use('/scheduler', scheduler);
 
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  console.log("In production")
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.API_PORT || 5000;
 
 app.listen(port, () => console.log(`App listening ${port}`));
