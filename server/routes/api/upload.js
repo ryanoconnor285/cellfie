30119091112c9180d9e999f083713349f816d614
Ryan O'Connor <rocsteady888@gmail.com>
2019-09-10 18:53:05 -0400

    separated scheduling

--- server/routes/api/upload.js
+++ server/routes/api/upload.js
@@ -6,7 +6,6 @@ const fileUpload = require("express-fileupload");
 const path = require("path");
 const fs = require("fs");
 
-
 router.use(fileUpload());
 
 // @route   GET upload/test
@@ -15,7 +14,7 @@ router.use(fileUpload());
 router.get("/test", (req, res) => res.json({ msg: "Upload Works" }));
 
 // @route   POST /upload
-// @desc    Tests upload route
+// @desc    Upload route
 // @access  Public
 router.post("/", (req, res) => {
   if (req.files === null) {
@@ -23,7 +22,7 @@ router.post("/", (req, res) => {
   }
 
   const file = req.files.file;
-  console.log(file)
+  console.log(file);
   // File ID with User ID and Unique Global ID (uniqid)
   const guid = uniqid();
   // TODO implement json web tokens
@@ -36,32 +35,14 @@ router.post("/", (req, res) => {
       console.error(err);
       return res.status(500).send(err);
     }
-    fs.appendFile(`${__dirname}/../../${fid}/in.csv`, file.data, function (err) {
+    fs.appendFile(`${__dirname}/../../${fid}/in.csv`, file.data, function(err) {
       if (err) throw err;
-      console.log('Saved!');
+      console.log("Saved!");
     });
-  })
-    // const model = "MT_recon_2_2_entrez.mat";
-    // const thresh = "-l";
-    // const percval = "-v";
-    // const type = "minmaxmean";
-    // const low = 25;
-    // const high = 75;
+  });
+  console.log(fid);
 
-    //   request(
-    //     `curl -X POST "http://cellfie2.renci.org:8080/tx-queue/2/scheduler/job" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"image\":\"krobasky/cellfiemodels:nooptV1\",\"command\":\"/bin/bash -c './execCellfie.sh -d tmp/in.csv -r\
-    // ${model} ${thresh} ${percval} -t ${type} --low ${low} --high ${high} >  tmp/output.tsv 2> tmp/err.tsv'\",\"mounts\":[{\"source\":\"${fid}\",\"target\":\"/CellFie/tmp\",\"type\":\"bind\",\"read_only\":false}]}"|sed 's/\"//g'`,
-    //     function(error, response, body) {
-    //       if (!error && response.statusCode == 200) {
-    //         // writing the response to a file named data.html
-    //         fs.writeFileSync("data.html", body);
-    //       }
-    //     }
-    //   );
-
-    console.log(fid);
-
-    res.json({ fileName: file.name, filePath: fid, guid: guid });
+  res.json({ fileName: file.name, filePath: fid, guid: guid });
 });
 
 // @route   GET upload/all
@@ -80,7 +61,7 @@ router.get("/all", (req, res) => {
     files.forEach(function(file) {
       console.log(file);
     });
-    res.json(files)
+    res.json(files);
   });
 });
 
